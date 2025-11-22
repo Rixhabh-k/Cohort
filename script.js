933f41746bc38151d035f7f2f38e88650ca31eda
Rishabh Khawar <kharwarrishabh891@gmail.com>
2025-11-22 22:53:43 +0530

    Custom Cursor

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
 var main = document.querySelector('main')
-var btn = document.querySelector('button')
+var cursor = document.querySelector('#cursor img')
 
-btn.addEventListener('click',()=>{
+main.addEventListener('mousemove',(val)=>{
     
-    var box = document.createElement('div');
-
-    let x = Math.random()*80
-    let y = Math.random()*80
-
-    let c1 = Math.floor(Math.random()*256)
-    let c2 = Math.floor(Math.random()*256)
-    let c3 = Math.floor(Math.random()*256)
-
-    let rot = Math.random()*360
-
-    box.style.height = '100px'
-    box.style.width = '100px'
-    box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
-    box.style.position = 'absolute'
-    box.style.top = x+'%'
-    box.style.left = y+'%'
-    box.style.rotate = rot+'deg'
-
-    main.append(box)
-    
+    cursor.style.top = val.y + 'px'
+    cursor.style.left = val.x + 'px'
     
 })
