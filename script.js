d9cbd4e712939311ce70fcde5819fe855c653373
Rishabh Khawar <kharwarrishabh891@gmail.com>
2025-11-22 15:51:31 +0530

    Random paper generator

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-var box = document.querySelector("#box")
+var main = document.querySelector('main')
 var btn = document.querySelector('button')
 
-btn.addEventListener('click',function(){
-    var c1 = Math.floor(Math.random()*256)
-    var c2 = Math.floor(Math.random()*256)
-    var c3 = Math.floor(Math.random()*256)
+btn.addEventListener('click',()=>{
+    
+    var box = document.createElement('div');
 
+    let x = Math.random()*80
+    let y = Math.random()*80
+
+    let c1 = Math.floor(Math.random()*256)
+    let c2 = Math.floor(Math.random()*256)
+    let c3 = Math.floor(Math.random()*256)
+
+    let rot = Math.random()*360
+
+    box.style.height = '100px'
+    box.style.width = '100px'
     box.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    box.style.position = 'absolute'
+    box.style.top = x+'%'
+    box.style.left = y+'%'
+    box.style.rotate = rot+'deg'
+
+    main.append(box)
+    
     
 })
