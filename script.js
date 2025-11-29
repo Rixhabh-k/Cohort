8c565818372c50a1b172058324164efa19b60f52
Rishabh Khawar <kharwarrishabh891@gmail.com>
2025-11-29 11:24:31 +0530

    Simple To Do App

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
-let p = document.querySelector('p')
+var input = document.querySelector('#task-input')
 
-let text = p.innerHTML
+var add = document.querySelector('#add-task')
 
-let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+var ul = document.querySelector('#ul-task-list')
 
-let iteration = 0
+add.addEventListener('click',()=>{
+    
+    var li = document.createElement('li')
+    
 
-function randomText(){
-    let str = text.split("").map((char, index)=>{
-        if(index<iteration){
-            return char
-        }
-        return character.split("")[Math.floor(Math.random()*52)]
-    }).join("")
+    li.innerHTML = `<span>${input.value.trim()}</span>
 
-    p.innerHTML = str
+                            <div class="task-actions">
+                                
+                                <button class="delete"><i class="ri-delete-bin-6-fill"></i></button>
+                            </div>`
 
-    iteration += 0.25
+    ul.appendChild(li);
 
- 
-if(iteration>= text.length){
-        clearInterval(str)
+    input.value = ""
+
+    // task delete logic
+
+    ul.addEventListener('click',(e)=>{
+        const deleteBtn = e.target.closest('.delete')
+
+        if (deleteBtn) {
+        const li = deleteBtn.closest('li');
+        if (li) li.remove();
     }
-    
+        
+    })
 
+    // task complete logic 
 
-}
-
-setInterval(function(){
-    randomText()
-},30)
-
+    var liFlag = 0;
 
     
+
+    li.addEventListener('click',()=>{
+        if(liFlag===0){
+            li.style.textDecoration = "line-through"
+            li.style.backgroundColor = "gray"
+            li.style.fontStyle = "italic"
+            liFlag = 1;
+        }else{
+            li.style.textDecoration = "none"
+            li.style.backgroundColor = "rgb(255, 186, 186)"
+            li.style.fontStyle = "normal"
+            liFlag = 0;
+        }
+    })
+                            
+})        
