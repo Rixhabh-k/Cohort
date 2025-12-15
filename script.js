d7fa628b04348d9252fd3fd2a0aa90b6ecd43344
Rishabh Khawar <kharwarrishabh891@gmail.com>
2025-12-15 20:55:52 +0530

    Weather App API
    created a weather forcast app through

--- script.js
+++ script.js
@@ -1,51 +1,103 @@
-function getUser() {
+let main = document.querySelector('main')
 
+function getWeather(city) {
+  fetch(`https://api.weatherapi.com/v1/current.json?key=c188cb9153324947b1d133536251412&q=${city}`)
 
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
 
-  fetch("https://randomuser.me/api/?results=3")
+      const oldWeatherData = document.querySelector(".weather-data");
+      if (oldWeatherData) {
+        oldWeatherData.remove();
+      }
 
-    .then((raw) => raw.json())
-    .then((data) => {
-      // console.log(data.results);
-      document.querySelector('main').innerHTML = "";
-      data.results.forEach(function (user) {
+      // target container (card ke andar append hoga)
+      const card = document.querySelector(".card");
 
+      // -------- weather-data --------
+      const weatherData = document.createElement("div");
+      weatherData.className = "weather-data";
 
-        const card = document.createElement("div");
-        card.classList.add("card");
+      // -------- temperature --------
+      const temperature = document.createElement("div");
+      temperature.className = "temperature";
 
+      const img = document.createElement("img");
+      img.src =
+        `${data.current.condition.icon}`;
+      img.alt = "";
 
-        const img = document.createElement("img");
-        img.src = user.picture.large;
-        img.alt = "";
+      const tempH1 = document.createElement("h1");
+      tempH1.innerText = `${data.current.temp_c} °C`;
 
+      const cityP = document.createElement("p");
+      cityP.innerText = `${data.location.name}`;
 
-        const info = document.createElement("div");
-        info.classList.add("info");
+      temperature.append(img, tempH1, cityP);
 
+      // -------- bottom --------
+      const bottom = document.createElement("div");
+      bottom.className = "bottom";
 
-        const h1 = document.createElement("h1");
-        h1.textContent = user.name.first + "" + user.name.first;
+      // humidity
+      const humidity = document.createElement("div");
+      humidity.className = "humidity";
 
+      const humidityH1 = document.createElement("h1");
+      humidityH1.innerText = `${data.current.humidity}%`;
 
-        const p = document.createElement("p");
-        p.textContent = user.email;
+      const humidityP = document.createElement("p");
+      humidityP.innerText = "Humidity";
 
+      humidity.append(humidityH1, humidityP);
 
-        info.appendChild(h1);
-        info.appendChild(p);
+      // wind
+      const wind = document.createElement("div");
+      wind.className = "wind";
 
-        card.appendChild(img);
-        card.appendChild(info);
+      const windH1 = document.createElement("h1");
+      windH1.innerText = `${data.current.wind_kph} kph`;
 
+      const windP = document.createElement("p");
+      windP.innerText = "Wind speed";
 
-        document.querySelector('main').append(card)
+      wind.append(windH1, windP);
 
+      // bottom append
+      bottom.append(humidity, wind);
 
-      })
+      // weather-data append
+      weatherData.append(temperature, bottom);
+
+      // -------- FINAL APPEND (IMPORTANT) --------
+      // search-box ke baad add kar rahe hain (same as HTML structure)
+      const searchBox = document.querySelector(".search-box");
+      searchBox.after(weatherData);
+
 
     })
+    .catch(function (err) {
+      alert("Please correct city name")
+
+    })
+
 }
 
-getUser();
+let search = document.querySelector('#search')
+
+let searchBtn = document.querySelector('#searchBtn')
+
+searchBtn.addEventListener("click", function () {
+  const city = search.value.trim();
+
+  if (city === "") return;
+
+  getWeather(city)
+
+  search.value = "";
+})
+
+
+
 
